7c127283d244e32cad020e059ef08515c8ca2ec3
Xu Wei <18422432+weixu000@users.noreply.github.com>
2023-04-10 00:36:48 +0800

    Use spaceship-prompt

--- prompt.zsh
+++ prompt.zsh
@@ -1,2 +1,5 @@
-source $ZDOTDIR/powerlevel10k/powerlevel10k.zsh-theme
-[[ ! -f $ZDOTDIR/.p10k.zsh ]] || source $ZDOTDIR/.p10k.zsh
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_EXIT_CODE_SHOW=true
+SPACESHIP_USER_SHOW='always'
+SPACESHIP_HOST_SHOW='always'
+source "$ZDOTDIR/spaceship-prompt/spaceship.zsh"
