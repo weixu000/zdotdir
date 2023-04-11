56fd80622005648e6e86f95efc8525f3cd124c0e
Xu Wei <18422432+weixu000@users.noreply.github.com>
2023-04-11 17:36:16 +0800

    Set prompt from scratch

--- prompt.zsh
+++ prompt.zsh
@@ -1,12 +1,23 @@
-fpath+=($ZDOTDIR/pure)
-autoload -U promptinit; promptinit
+autoload -Uz vcs_info
+precmd_functions+=(vcs_info)
 
-zstyle :prompt:pure:git:stash show yes
-zstyle :prompt:pure:git:fetch only_upstream yes
+zstyle ':vcs_info:git:*' formats '%b %u%c'
+zstyle ':vcs_info:git:*' actionformats '[%b|%a]%u%c'
 
-prompt pure
-PROMPT='%F{white}%T '$PROMPT
+setopt prompt_subst
 
-PURE_PROMPT_SYMBOL='»'
-PURE_GIT_DOWN_ARROW='↓'
-PURE_GIT_UP_ARROW='↑'
+PS1='%F{cyan}%n@%m:%F{green}%~ %F{yellow}${vcs_info_msg_0_}%F{reset}%# '
+RPS1='${ZSH_COMMAND_TIME} %(?.. %F{red}%?) %F{magenta}%t%f'
+
+_command_time_preexec() {
+  _command_time_timer=''
+  SECONDS=0
+}
+
+_command_time_precmd() {
+  [[ -v _command_time_timer && $SECONDS -gt ${ZSH_COMMAND_TIME_MIN_SECONDS:-5} ]] && ZSH_COMMAND_TIME="${SECONDS}s" || unset ZSH_COMMAND_TIME
+  unset _command_time_timer
+}
+
+precmd_functions+=(_command_time_precmd)
+preexec_functions+=(_command_time_preexec)
