5f5fa1d35d9da5fef58150eeac4129590e441675
Xu Wei <18422432+weixu000@users.noreply.github.com>
2023-05-14 11:45:54 +0800

    Auto rehash

--- completion.zsh
+++ completion.zsh
@@ -3,3 +3,5 @@ source $ZDOTDIR/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 fpath=($ZDOTDIR/zsh-completions/src $ZDOTDIR/conda-zsh-completion $fpath)
 
 source $ZDOTDIR/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+zstyle ':completion:*' rehash true
